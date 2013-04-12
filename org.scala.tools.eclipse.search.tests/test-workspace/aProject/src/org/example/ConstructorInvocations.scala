54b6c3ce44d9ebf37ca5219fbb0a554bcf1bd0f2
Mads Hartmann Jensen <mads379@gmail.com>
2013-04-12 16:11:54 +0200

    Record occurrences of constructors
    The occurrence collector now records occurrences of constructors;
    that is both definitions and invocations. Test have been added for
    the various ways you can define constructors.
    
    Fix #1001622

---
+++ org.scala.tools.eclipse.search.tests/test-workspace/aProject/src/org/example/ConstructorInvocations.scala
@@ -0,0 +1,9 @@
+class ConstructorInvocations {
+
+}
+
+object ConstructorInvocations {
+
+  def apply() = new ConstructorInvocations
+
+}
---
+++ org.scala.tools.eclipse.search.tests/test-workspace/aProject/src/org/example/ExplicitConstructor.scala
@@ -0,0 +1,5 @@
+class ExplicitConstructor {
+
+  def this(s: String) = this
+
+}
