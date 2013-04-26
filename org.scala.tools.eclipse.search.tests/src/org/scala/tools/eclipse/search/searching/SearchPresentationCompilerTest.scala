adb778ccbd186adfc6374900957d6277f1e54212
Mads Hartmann Jensen <mads379@gmail.com>
2013-04-26 21:54:09 +0200

    Improved test code of SearchPresentationCompilerTest
    This makes it easier to produce new tests and makes
    them more robust (i.e. we don't have to pass the
    location manually)
    
    Fixes #1001687

--- org.scala.tools.eclipse.search.tests/src/org/scala/tools/eclipse/search/searching/SearchPresentationCompilerTest.scala
+++ org.scala.tools.eclipse.search.tests/src/org/scala/tools/eclipse/search/searching/SearchPresentationCompilerTest.scala
@@ -1,33 +1,76 @@
 package org.scala.tools.eclipse.search.searching
 
+import scala.tools.eclipse.EclipseUserSimulator
+import scala.tools.eclipse.ScalaProject
+import scala.tools.eclipse.javaelements.ScalaCompilationUnit
 import scala.tools.eclipse.testsetup.TestProjectSetup
-import org.junit.Test
-import org.scala.tools.eclipse.search.TestUtil
+import org.junit._
 import org.junit.Assert._
+import org.scala.tools.eclipse.search.TestUtil
 
 class SearchPresentationCompilerTest {
 
   import SearchPresentationCompilerTest._
   import SearchPresentationCompiler._
 
-  @Test def canGetSymbolAtLocation {
-    val unit = scalaCompilationUnit(mkPath("org", "example", "ExampleWithSymbol.scala"))
-    unit.withSourceFile { (cu, pc) =>
-      val symbol = pc.symbolAt(Location(unit, 34), cu).get
-      assertEquals("x", pc.askOption( () => symbol.nameString ).get)
-    }(fail("withSourceFile call failed unexpectedly"))
+  private final val CaretMarker = '|'
+
+  protected val simulator = new EclipseUserSimulator
+  private var project: ScalaProject = _ // A project is needed, otherwise we get NPE when using simulator.
+
+  @Before
+  def createProject() {
+    project = simulator.createProjectInWorkspace("search-presentation-compiler-test", true)
   }
 
-  @Test def noneIfAskedForWrongLocation {
-    val unit = scalaCompilationUnit(mkPath("org", "example", "ExampleWithSymbol.scala"))
-    unit.withSourceFile { (cu, pc) =>
-      val symbol = pc.symbolAt(Location(unit, 108), cu) // 108 doesn't exist.
-      assertEquals(true, symbol.isEmpty)
-    }(fail("withSourceFile call failed unexpectedly"))
+  @After
+  def deleteProject() {
+    project.underlying.delete(true, null)
   }
 
+  @Test
+  def canGetSymbolAtLocation {
+    document {"""
+      class ExampleWithSymbol {
+        val |x: Int = 42
+      }
+    """
+    } expectedSymbolNamed(Some("x"))
+  }
+
+  @Test
+  def noneIfAskedForWrongLocation {
+    document {"""
+      class ExampleWithSymbol {
+        val x: Int = 42
+      }
+      |
+    """
+    } expectedSymbolNamed(None)
+  }
+
+  private def scalaSourceFile(code: String): ScalaCompilationUnit = {
+    val emptyPkg = simulator.createPackage("")
+    simulator.createCompilationUnit(emptyPkg, "A.scala", code.stripMargin).asInstanceOf[ScalaCompilationUnit]
+  }
+
+  private def document(text: String) = new {
+    def expectedSymbolNamed(nameOpt: Option[String]): Unit = {
+      val caret: Int = {
+        val offset = text.indexOf(CaretMarker)
+        if (offset == -1) fail(s"Could not locate caret position marker '${CaretMarker}' in test.")
+        offset
+      }
+      val cleanedText = text.filterNot(_ == CaretMarker).mkString
+      val cu = scalaSourceFile(cleanedText)
+      cu.withSourceFile { (sf, pc) =>
+        val symbol = pc.symbolAt(Location(cu, caret), sf)
+        assertEquals(nameOpt, symbol.map(_.nameString))
+      } (fail("Couldn't get Scala source file"))
+    }
+  }
 }
 
 object SearchPresentationCompilerTest
-  extends TestProjectSetup("SearchPresentationCompilerTest", bundleName= "org.scala.tools.eclipse.search.tests") 
+  extends TestProjectSetup("SearchPresentationCompilerTest", bundleName= "org.scala.tools.eclipse.search.tests")
      with TestUtil
