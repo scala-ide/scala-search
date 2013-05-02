5002b578d437d20a89a7fa7dff21d61c3e655705
Mads Hartmann Jensen <mads379@gmail.com>
2013-05-02 12:30:42 +0200

    Provide an API for comparing method symbols
    The SearchPresentationCompiler can now compare two symbols and
    check if they describe the same method.
    
    This works even for symbols that have been obtained from seperate
    presentation compilers; we use Importers to take care of that.
    
    fixes #1001683

--- org.scala.tools.eclipse.search/src/org/scala/tools/eclipse/search/searching/SearchPresentationCompiler.scala
+++ org.scala.tools.eclipse.search/src/org/scala/tools/eclipse/search/searching/SearchPresentationCompiler.scala
@@ -15,13 +15,19 @@ object SearchPresentationCompiler extends HasLogger {
    */
   implicit class SearchPresentationCompiler(val pc: ScalaPresentationCompiler) {
 
+    // ADT used to express the result of symbolAt.
+    sealed abstract class SymbolRequest
+    case class FoundSymbol(symbol: pc.Symbol) extends SymbolRequest
+    case object MissingSymbol extends SymbolRequest
+    case object NotTypeable extends SymbolRequest
+
     /**
      * Get the symbol of the entity at a given location in a file.
      *
      * The source file needs to be loaded before invoking this method. This can be
      * achieved by invoking `pc.askReload(..)`.
      */
-    def symbolAt[A](loc: Location, cu: SourceFile): Option[pc.Symbol] = {
+    def symbolAt[A](loc: Location, cu: SourceFile): SymbolRequest = {
       val typed = new Response[pc.Tree]
       val pos = new OffsetPosition(cu, loc.offset)
       pc.askTypeAt(pos, typed)
@@ -29,16 +35,71 @@ object SearchPresentationCompiler extends HasLogger {
         tree => filterNoSymbols(tree.symbol),
         err => {
           logger.debug(err)
-          None
+          NotTypeable
         })
     }
 
-    private def filterNoSymbols(sym: pc.Symbol): Option[pc.Symbol] = {
-      if (sym == null || sym == pc.NoSymbol) {
-        None
+    private def filterNoSymbols(sym: pc.Symbol): SymbolRequest = {
+      if (sym == null) {
+        MissingSymbol // Symbol is null if there isn't any node at the given location (I think!)
+      } else if (sym == pc.NoSymbol) {
+        NotTypeable // NoSymbol if we can't typecheck the given node.
       } else {
-        Some(sym)
+        FoundSymbol(sym)
       }
+    }
+
+    /**
+     * Import a symbol from one presentation compiler into another. This is required
+     * before you can compare two symbols originating from different presentation
+     * compiler instance.
+     *
+     * TODO: Is the imported symbol discarded again, or does this create a memory leak?
+     *       Ticket #1001703
+     */
+    def importSymbol(spc: SearchPresentationCompiler)(s: spc.pc.Symbol): pc.Symbol = {
+      // https://github.com/scala/scala/blob/master/src/reflect/scala/reflect/api/Importers.scala
+      val importer0 = pc.mkImporter(spc.pc)
+      val importer = importer0.asInstanceOf[pc.Importer { val from: spc.pc.type }]
+      importer.importSymbol(s)
+    }
+
+    /**
+     * Check if symbols `s1` and `s2` are describing the same method. We consider two
+     * methods to be the same if
+     *
+     *  - They have the same name
+     *  - s1.owner and s2.owner are in the same hierarchy
+     *  - They have the same type signature or one is overriding the other
+     */
+    def isSameMethod(s1: pc.Symbol, s2: pc.Symbol): Boolean = {
+      pc.askOption { () =>
+
+        lazy val isInHiearchy = s1.owner.isSubClass(s2.owner) || s2.owner.isSubClass(s1.owner)
+
+        lazy val hasSameName = {
+          def getName(s: pc.Symbol)= {
+            if(pc.nme.isLocalName(s.name))
+              pc.nme.localToGetter(s.name.toTermName)
+            else s.name
+          }
+          getName(s1) == getName(s2)
+        }
+
+        lazy val hasSameTypeSignature = s1.typeSignature =:= s2.typeSignature
+
+        // If s1 and s2 are defined in the same class the owner will be the same
+        // thus s1.overriddenSymbol(S2.owner) will actually return s1.
+        lazy val isOverridden = s1.owner != s2.owner &&
+                                (s1.overriddenSymbol(s2.owner) != pc.NoSymbol ||
+                                s2.overriddenSymbol(s1.owner) != pc.NoSymbol)
+
+        (s1 == s2) || // Fast-path.
+        hasSameName &&
+        isInHiearchy &&
+        (hasSameTypeSignature || isOverridden)
+
+      } getOrElse false
     }
 
   }
