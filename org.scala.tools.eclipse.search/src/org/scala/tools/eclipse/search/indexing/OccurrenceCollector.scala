21fdc52929b58391115e23bd1eeb237f677091de
François Garillot <francois@garillot.net>
2014-09-24 18:32:39 +0200

    Regularize usage of EclipseUtils, IScalaPresentationCompiler.RichResponse

--- org.scala.tools.eclipse.search/src/org/scala/tools/eclipse/search/indexing/OccurrenceCollector.scala
+++ org.scala.tools.eclipse.search/src/org/scala/tools/eclipse/search/indexing/OccurrenceCollector.scala
@@ -36,7 +36,7 @@ object OccurrenceCollector extends HasLogger {
         new InvalidPresentationCompilerException(
             s"Couldn't get source file for ${file.file.path}"))
 
-    import org.scalaide.core.compiler.IScalaPresentationCompiler.Implicits._
+    import org.scalaide.core.compiler.IScalaPresentationCompiler.Implicits.RichResponse
 
     if (file.exists) {
       file.withSourceFile( (source, pcompiler) => {
